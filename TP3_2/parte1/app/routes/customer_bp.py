9cd91b584f49bcab2809991cfe81969d03250db6
Fer_Stock <monterofernandoe@gmail.com>
2023-08-26 09:25:44 -0300

    Ej4 bluepints

--- TP3_2/parte1/app/routes/customer_bp.py
+++ TP3_2/parte1/app/routes/customer_bp.py
@@ -6,4 +6,6 @@ customer_bp = Blueprint('customer_bp', __name__)
 customer_bp.route('/customer', methods=['POST'])(CustomerController.create_customer)
 customer_bp.route('/customers/<int:customer_id>', methods=['GET'])(CustomerController.get_customer)
 customer_bp.route('/customers', methods=['GET'])(CustomerController.get_customers)
+customer_bp.route('/customers/<int:customer_id>', methods=['PUT'])(CustomerController.update_customer)
+
 
